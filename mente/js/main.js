988920e55ae22bc039a29a2f1dc7e26f42f0f485
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2022-10-06 03:55:01 -0700

    78

--- mente/js/main.js
+++ mente/js/main.js
@@ -136,35 +136,49 @@
 // }
 // resizeListener();
 // $(window).resize(resizeListener);
-// // faq start
-// const tabBtn = document.querySelectorAll(".tabBtn");
-// const tabEvent = document.querySelectorAll(".tabEvent");
-// tabBtn.forEach((e) => {
-//   onTabClick(tabBtn, tabEvent, e);
-// });
-// function onTabClick(tabBtns, tabItems, item) {
-//   item.addEventListener("click", function (e) {
-//     let currentBtn = item;
-//     let tabId = currentBtn.getAttribute("data-tab");
-//     let currentTab = document.querySelector(tabId);
-//     if (currentBtn.classList.contains("active")) {
-//       console.log("now active");
-//       const faq = currentBtn.parentElement.querySelector(".tabEvent");
-//       if (faq) {
-//         faq.classList.remove("active");
-//         currentBtn.classList.remove("active");
-//       }
-//     } else if (!currentBtn.classList.contains("active")) {
-//       tabBtns.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
+// faq start
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      console.log("now active");
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
 
-//       tabItems.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-//       currentBtn.classList.add("active");
-//       currentTab.classList.add("active");
-//     }
-//   });
-// }
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
 // // faq end
+
+
+ $(function () {
+  $(".testimonials__inner-slider").slick({
+    dots: true,
+    infinite: true,
+    centerMode: false,
+    fade: true,
+    arrows:true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
